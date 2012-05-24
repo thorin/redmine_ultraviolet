dc152258d1c6184cd8b4e2cb042e400a69728ceb
root <root@vforge2.lyon.cemagref.fr>
2012-05-24 13:46:25 +0200

    Do not fail at startup if UserCustomField does exist yet.

--- init.rb
+++ init.rb
@@ -9,37 +9,39 @@ Redmine::Plugin.register :redmine_ultraviolet do
   description "Uses Textmate's syntaxes highlighters to highlight files in the source code repository."
   version "0.0.3"
 
-  return unless UserCustomField.table_exists?
+  if UserCustomField.table_exists?
 
-  all_themes = Uv.themes.sort
-  default_theme = all_themes.first
+    all_themes = Uv.themes.sort
+    default_theme = all_themes.first
 
-  # Copy theme stylesheets
-  css_directory = File.join(Engines.public_directory, 'redmine_ultraviolet', 'stylesheets')
-  Uv.path.each do |dir|
-    Engines.mirror_files_from File.join( dir, "render", "xhtml", "files", "css" ), css_directory
-  end
+    # Copy theme stylesheets
+    css_directory = File.join(Engines.public_directory, 'redmine_ultraviolet', 'stylesheets')
+    Uv.path.each do |dir|
+      Engines.mirror_files_from File.join( dir, "render", "xhtml", "files", "css" ), css_directory
+    end
 
-  # Create or update a user custom field to hold user preference theme
-  custom_field = UserCustomField.find_by_name(UvSyntaxHighlighting::CUSTOM_FIELD_NAME)
-  unless custom_field
-    UserCustomField.create(
-      :name             => UvSyntaxHighlighting::CUSTOM_FIELD_NAME,
-      :default_value    => default_theme,
-      :possible_values  => all_themes,
-      :field_format     => 'list',
-      :is_required      => true
-    )
-  else
-    # Keep in sync with available themes
-    custom_field.default_value = default_theme
-    custom_field.possible_values = all_themes
-    custom_field.save if custom_field.changed?
-  end
+    # Create or update a user custom field to hold user preference theme
+    custom_field = UserCustomField.find_by_name(UvSyntaxHighlighting::CUSTOM_FIELD_NAME)
+    unless custom_field
+      UserCustomField.create(
+        :name             => UvSyntaxHighlighting::CUSTOM_FIELD_NAME,
+        :default_value    => default_theme,
+        :possible_values  => all_themes,
+        :field_format     => 'list',
+        :is_required      => true
+      )
+    else
+      # Keep in sync with available themes
+      custom_field.default_value = default_theme
+      custom_field.possible_values = all_themes
+      custom_field.save if custom_field.changed?
+    end
 
-  # Ensure we are always using our highlighter
-  Dispatcher.to_prepare do
-    Redmine::SyntaxHighlighting.highlighter = UvSyntaxHighlighting
+    # Ensure we are always using our highlighter
+    Dispatcher.to_prepare do
+      Redmine::SyntaxHighlighting.highlighter = UvSyntaxHighlighting
+    end
+
   end
 
 end
