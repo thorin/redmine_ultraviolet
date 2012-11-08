45137b7885746867cbe05f197d2ca649f4f03a54
Adirelle <adirelle@gmail.com>
2012-11-08 17:18:19 +0100

    Embedded modified Ultraviolet themes so they fit with Redmine CSS3 classes.

--- init.rb
+++ init.rb
@@ -11,12 +11,6 @@ Redmine::Plugin.register :redmine_ultraviolet do
 
   if UserCustomField.table_exists?
 
-    # Copy theme stylesheets
-    css_directory = File.join(Engines.public_directory, 'redmine_ultraviolet', 'stylesheets')
-    Uv.path.each do |dir|
-      Engines.mirror_files_from File.join( dir, "render", "xhtml", "files", "css" ), css_directory
-    end
-
     # Create or update a user custom field to hold user preference theme
     custom_field = UserCustomField.find_by_name(UvSyntaxHighlighting::CUSTOM_FIELD_NAME)
     unless custom_field
