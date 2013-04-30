62e6b76d8e06a72acb2c81c60cf8bd2e910806cb
jbbarth <jeanbaptiste.barth@gmail.com>
2013-04-30 21:15:55 +0200

    Reorganize init.rb file

--- init.rb
+++ init.rb
@@ -4,36 +4,34 @@ require_dependency 'uv_extension'
 require_dependency 'uv_syntax_highlighting'
 require_dependency 'uv_view_hook_listener'
 
-ActionDispatch::Callbacks.to_prepare do
-  # Ensure we are always using our highlighter
-  Redmine::SyntaxHighlighting.highlighter = 'UvSyntaxHighlighting'
-end
-
 Redmine::Plugin.register :redmine_ultraviolet do
   name "Redmine Ultraviolet Syntax highlighting plugin"
   author "Chris Gahan"
   description "Uses Textmate's syntaxes highlighters to highlight files in the source code repository."
   version "0.0.3"
+end
 
-  if UserCustomField.table_exists?
+# Patches
+ActionDispatch::Callbacks.to_prepare do
+  # Ensure we are always using our highlighter
+  Redmine::SyntaxHighlighting.highlighter = 'UvSyntaxHighlighting'
+end
 
-    # Create or update a user custom field to hold user preference theme
-    custom_field = UserCustomField.find_by_name(UvSyntaxHighlighting::CUSTOM_FIELD_NAME)
-    unless custom_field
-      UserCustomField.create(
-        :name             => UvSyntaxHighlighting::CUSTOM_FIELD_NAME,
-        :default_value    => UvSyntaxHighlighting.default_theme,
-        :possible_values  => UvSyntaxHighlighting.all_themes,
-        :field_format     => 'list',
-        :is_required      => true
-      )
-    else
-      # Keep in sync with available themes
-      custom_field.default_value = UvSyntaxHighlighting.default_theme
-      custom_field.possible_values = UvSyntaxHighlighting.all_themes
-      custom_field.save if custom_field.changed?
-    end
-
+# Create or update a user custom field to hold user preference theme
+if UserCustomField.table_exists?
+  custom_field = UserCustomField.find_by_name(UvSyntaxHighlighting::CUSTOM_FIELD_NAME)
+  unless custom_field
+    UserCustomField.create(
+      :name             => UvSyntaxHighlighting::CUSTOM_FIELD_NAME,
+      :default_value    => UvSyntaxHighlighting.default_theme,
+      :possible_values  => UvSyntaxHighlighting.all_themes,
+      :field_format     => 'list',
+      :is_required      => true
+    )
+  else
+    # Keep in sync with available themes
+    custom_field.default_value = UvSyntaxHighlighting.default_theme
+    custom_field.possible_values = UvSyntaxHighlighting.all_themes
+    custom_field.save if custom_field.changed?
   end
-
 end
